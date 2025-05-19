7157deeda92ef328ed3c656c1fc1d12ebb48efbf
e-maleki <em2995@pleasantonusd.net>
2025-05-19 08:29:59 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// This is a simple starter JavaScript program
+
+// Function to say hello
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Call the functions
+greet("World");
+let result = add(5, 3);
+console.log("5 + 3 = " + result);
